6f78e9ed6b1131f9b1f08f3e84d959d550ecf26c
陈材华 <mishe@163.com>
2016-11-03 17:12:19 +0800

    Update helper.js

--- component/helper.js
+++ component/helper.js
@@ -236,7 +236,8 @@ $.extend({
         v = v.toString().split('.');
         return (+v[0]).toLocaleString() + '.' + (v[1] >= 01 ? v[1] : '00');
     },
-    isIDCard:function(code){
+    is
+    :function(code){
         var city={11:"北京",12:"天津",13:"河北",14:"山西",15:"内蒙古",21:"辽宁",22:"吉林",23:"黑龙江 ",31:"上海",32:"江苏",33:"浙江",34:"安徽",35:"福建",36:"江西",37:"山东",41:"河南",42:"湖北 ",43:"湖南",44:"广东",45:"广西",46:"海南",50:"重庆",51:"四川",52:"贵州",53:"云南",54:"西藏 ",61:"陕西",62:"甘肃",63:"青海",64:"宁夏",65:"新疆",71:"台湾",81:"香港",82:"澳门",91:"国外 "};
         var tip = "";
         var pass= true;
@@ -335,14 +336,73 @@ $.extend({
                             password=[];
                             $(opt.pageContain).html(passworkBox);
                         }
+var defaultSetting={
+                type:'popup', //type：popup  page
+                pageContain:'', //type=page 时有效；密码框的展示区域
+                value:0,
+                callback:function(){}
+            },
+            opt=$.extend(true,defaultSetting,opt),
+            //https://app.91yaowang.com/weizhan/pages/member/forgetPayPwd
+            forgotUrl=location.origin + "/pages/member/forgetPayPwd",
+            oldPassword,
+            password=[],
+            win;
+        if(opt.type=='popup'){
+            win=$(template());
+        }else{
+            passworkBox=$(passwordBox());
+            $(opt.pageContain).html(passworkBox);
+            win=$(keyBoard())
+        }
+
+        $('body').append(win);
+        
+        win.on('touchmove',function(e){
+            e.preventDefault();
+        }).on('tap','.key-one',function(e){
+            var obj=$(e.currentTarget),
+                value=obj.attr('data-num');
+            if(value=='reset'){
+                password=[];
+            }else if(value=='backspace'){
+                password.pop();
+            }else{
+                password.push(value);
+            }
+            renderPassword();
+            if(password.length>=6){
+                if(opt.type=='popup') {
+                    setTimeout(function () {
+                        opt.callback(password.join(''));
+                        win.off().remove();
+                    }, 300);
+                }else{
+                    if(!oldPassword) {
+                        oldPassword = password.join('');
+                        passworkBox = $(passwordBox(1));
+                        password=[];
+                        $(opt.pageContain).html(passworkBox);
+                    }else{
+                        if(oldPassword==password.join('')){
+                            opt.callback(password.join(''));
+                            win.off().remove();
+                        }else{
+                            $.toast('两次密码输入不一致');
+                            passworkBox = $(passwordBox());
+                            password=[];
+                            oldPassword=0;
+                            password=[];
+                            $(opt.pageContain).html(passworkBox);
+                        }
                     }
                 }
             }
-        }).on('click','.cancel-btn',function(){
+        }).on('tap','.cancel-btn',function(){
             win.off().remove();
             opt.callback('');
         });
-
+        
         function renderPassword(){
             var len=password.length,
                 obj=win;
@@ -354,16 +414,16 @@ $.extend({
                 obj.find('.key-value').eq(i).html('●');
             }
         }
-
+        
         function passwordBox(bl){
             var title=bl?'请再次输入交易密码':'请设置交易密码';
-
+        
             return '<div class="trade-password" style="position:inherit;background-color:inherit;padding-bottom:50px"><h3 class="trade-password-title" style="border-bottom:inherit">'+title+'</h3>'
                 +'<div class="trade-box" style="background-color: #fff"><span class="key-value"></span><span class="key-value"></span>'
                 +'<span class="key-value"></span><span class="key-value"></span><span class="key-value"></span>'
                 +'<span class="key-value"></span></div></div>';
         }
-
+        
         function keyBoard(){
             return '<div class="keyboard-box">'
                 +'<span class="key-one" data-num="1">1</span><span class="key-one" data-num="2">2</span>'
@@ -373,12 +433,12 @@ $.extend({
                 +'<span class="key-one" data-num="9">9</span><span class="icons key-one reset" data-num="reset"></span>'
                 +'<span class="key-one" data-num="0">0</span><span class="icons key-one backspace" data-num="backspace"></span></div>';
         }
-
+        
         function template(){
             var str='<div class="password-mask"><div class="trade-password"><h3 class="trade-password-title">请输入摇旺交易密码</h3>';
             if(opt.value>0)
                 str+='<p class="trade-tip">交易金额：<span class="trade-num">￥'+opt.value+'元</span></p>'
-
+        
             str+='<div class="trade-box"><span class="key-value"></span><span class="key-value"></span>'
                 +'<span class="key-value"></span><span class="key-value"></span><span class="key-value"></span>'
                 +'<span class="key-value"></span></div><a href="'+forgotUrl+'" class="forgot-password-btn">忘记密码</a>'
